6df2b10137630900d5bdbd79d69b44a92722c6cf
Jackson Brown <jacksonb@alleninstitute.org>
2019-04-12 22:48:39 -0700

    Add `get_file` to FileStore ABC

--- cdptools/file_stores/file_store.py
+++ cdptools/file_stores/file_store.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Union
 
 ###############################################################################
 
@@ -9,9 +11,16 @@ from abc import ABC, abstractmethod
 class FileStore(ABC):
 
     @abstractmethod
-    def store_file(self, file: str, **kwargs) -> str:
+    def store_file(self, file: Union[str, Path]) -> str:
         """
         Store a file.
         """
 
         return ""
+
+    @abstractmethod
+    def get_file(self, file_id: str) -> Path:
+        """
+        Get a file by id.
+        """
+        return Path("/root/.local/path/to/file/id.mp4")

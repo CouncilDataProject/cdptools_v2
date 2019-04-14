45991df0c4d5d358d16b7ae5c67c85e4deff02b1
Jackson Brown <jacksonb@alleninstitute.org>
2019-04-14 00:46:15 -0700

    Use ProcessPoolExecutor instead of ThreadPoolExecutor per event

--- cdptools/pipelines/event_pipeline.py
+++ cdptools/pipelines/event_pipeline.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from concurrent.futures import ThreadPoolExecutor
+# For testing
+# import random
+
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
 import json
 import logging
 from pathlib import Path
@@ -31,11 +35,10 @@ class EventPipeline(Pipeline):
         with open(config_path, "r") as read_in:
             self.config = json.load(read_in)
 
-        # Get cron schedule
-        self.cron = self.config.get("cron_schedule")
+        # Get workers
         self.n_workers = self.config.get("max_synchronous_jobs")
 
-        # Load modules
+        # Load event scraper
         self.event_scraper = super().load_custom_object(
             module_path=self.config["event_scraper"]["module_path"],
             object_name=self.config["event_scraper"]["object_name"],
@@ -46,46 +49,53 @@ class EventPipeline(Pipeline):
             object_name=self.config["database"]["object_name"],
             object_kwargs=self.config["database"].get("object_kwargs", {})
         )
-        self.file_store = super().load_custom_object(
-            module_path=self.config["file_store"]["module_path"],
-            object_name=self.config["file_store"]["object_name"],
-            object_kwargs=self.config["file_store"].get("object_kwargs", {})
-        )
-        self.audio_splitter = super().load_custom_object(
-            module_path=self.config["audio_splitter"]["module_path"],
-            object_name=self.config["audio_splitter"]["object_name"],
-            object_kwargs=self.config["audio_splitter"].get("object_kwargs", {})
-        )
-        # self.sr_model = super().load_custom_object(
-        #     module_path=self.config["speech_recognition_model"]["module_path"],
-        #     object_name=self.config["speech_recognition_model"]["object_name"],
-        #     object_kwargs=self.config["speech_recognition_model"].get("object_kwargs", {})
-        # )
-        log.info("=" * 80)
 
-    def process_event(self, event: Dict) -> str:
+    @staticmethod
+    def process_event(event: Dict, config: Dict, module_loader: callable) -> str:
         # Get event key
         key = event["key"]
+
+        # Initialize all modules
+        database = module_loader(
+            module_path=config["database"]["module_path"],
+            object_name=config["database"]["object_name"],
+            object_kwargs={**config["database"].get("object_kwargs", {}), **{"name": f"db_{key}"}}
+        )
+        file_store = module_loader(
+            module_path=config["file_store"]["module_path"],
+            object_name=config["file_store"]["object_name"],
+            object_kwargs=config["file_store"].get("object_kwargs", {})
+        )
+        audio_splitter = module_loader(
+            module_path=config["audio_splitter"]["module_path"],
+            object_name=config["audio_splitter"]["object_name"],
+            object_kwargs=config["audio_splitter"].get("object_kwargs", {})
+        )
+        sr_model = module_loader(
+            module_path=config["speech_recognition_model"]["module_path"],
+            object_name=config["speech_recognition_model"]["object_name"],
+            object_kwargs=config["speech_recognition_model"].get("object_kwargs", {})
+        )
+
+        # Begin
         try:
             # Check event already exists
-            if self.database.get_event(key):
+            if database.get_event(key):
                 log.debug(f"Event already exists: {key}")
             else:
-                log.debug(f"Processing event: {key}")
+                log.info(f"Processing event: {key}")
 
                 # Check for video file
                 video_suffix = event["video_url"].split(".")[-1]
                 try:
-                    local_video_path = self.file_store.get_file(key, filename=f"video.{video_suffix}")
+                    local_video_path = file_store.get_file(key, filename=f"video.{video_suffix}")
                 except FileNotFoundError:
                     # Create local copy of video
-                    log.debug("Beginning video copy...")
-                    local_video_path = self.file_store.store_file(
+                    local_video_path = file_store.store_file(
                         file=event["video_url"],
                         key=key,
                         save_name=f"video.{video_suffix}"
                     )
-                    log.debug(f"Stored video at: {local_video_path}")
 
                 # Check for audio file
                 try:
@@ -97,11 +107,18 @@ class EventPipeline(Pipeline):
                         audio_save_path=local_video_path.parent / "audio.raw"
                     )
 
-                # # Transcribe audio to text
-                # transcript = self.sr_model.transcribe(local_paths["audio"])
+                # Check for transcript
+                # try:
+                #     local_transcript_path = file_store.get_file(key=key, filename="initial_transcript.txt")
+                # except FileNotFoundError:
+                #     # Transcribe audio to text
+                #     transcript = sr_model.transcribe(
+                #         audio_read_path=local_audio_path,
+                #         transcript_save_path=local_audio_path.parent / f"initial_transcript.txt"
+                #     )
 
                 # Store event
-                event = self.database.upload_event(event)
+                event = database.upload_event(event)
         except Exception as e:
             # Create error
             to_store_error = {
@@ -111,25 +128,25 @@ class EventPipeline(Pipeline):
             }
             # Upload
             log.info("Something went wrong... Uploading error details.")
-            self.database.upload_error(to_store_error)
+            database.upload_error(to_store_error)
 
-        # Finish
-        log.info(f"Finished processing event: {key}")
+        # Update progress
+        log.info(f"Completed processing for event: {key}")
 
     def run(self):
-        # TODO: wrap this in a scheduler
+        # Get events
+        log.info("Starting event processing.")
+        events = self.event_scraper.get_events()
 
-        try:
-            # Get events
-            log.info("Beginning event collection.")
-            events = self.event_scraper.get_events()[:3]
+        # For testing
+        # events = random.sample(events, 1)
 
-            # Multithread each event found
-            with ThreadPoolExecutor(self.n_workers) as exe:
-                exe.map(self.process_event, events)
+        # Initialize partial
+        process_func = partial(self.process_event, config=self.config, module_loader=super().load_custom_object)
 
-            log.info(f"Completed event processing.")
+        # Multiprocess each event found
+        with ProcessPoolExecutor(self.n_workers) as exe:
+            exe.map(process_func, events)
 
-        except Exception as e:
-            # TODO: push exception log to database
-            raise e
+        log.info("Completed event processing.")
+        log.info("=" * 80)

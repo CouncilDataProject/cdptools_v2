5f167f4537c22e7c6b366b0eee3d4b479881e297
Jackson Brown <jacksonb@alleninstitute.org>
2019-04-16 00:25:18 -0700

    Resolves [gh-6], resolves [gh-7], and resolves [gh-8]

--- cdptools/file_stores/gcs_file_store.py
+++ cdptools/file_stores/gcs_file_store.py
@@ -23,20 +23,34 @@ log = logging.getLogger(__file__)
 
 class GCSFileStore(FileStore):
 
-    def __init__(self, credentials_path: Union[str, Path], bucket_uri: str, **kwargs):
+    def __init__(self, credentials_path: Union[str, Path], bucket_name: str, **kwargs):
         # Resolve credentials
         self.credentials_path = Path(credentials_path).resolve(strict=True)
 
         # Initialize client
         self.client = storage.Client.from_service_account_json(self.credentials_path)
-        self.bucket = self.client.get_bucket(bucket_uri)
+        self.bucket = self.client.get_bucket(bucket_name)
 
-    def store_file(
+    def get_file_uri(self, filename: Union[str, Path], **kwargs) -> str:
+        # Resolve path
+        filename = Path(filename).resolve().name
+
+        # Create blob
+        blob = self.bucket.blob(filename)
+
+        # Check if file exists
+        if blob.exists():
+            return f"gs://{self.bucket.name}/{filename}"
+        else:
+            raise FileNotFoundError(filename)
+
+    def upload_file(
         self,
         filepath: Union[str, Path],
         save_name: Optional[str] = None,
         content_type: Optional[str] = None,
-        remove: bool = False
+        remove: bool = False,
+        **kwargs,
     ) -> str:
         # Resolve the path to enforce path complete
         filepath = Path(filepath).resolve(strict=True)
@@ -47,7 +61,7 @@ class GCSFileStore(FileStore):
 
         # Try to get the file first
         try:
-            return self.get_file(filename=save_name)
+            return self.get_file_uri(filename=save_name)
         except FileNotFoundError:
             pass
 
@@ -72,15 +86,26 @@ class GCSFileStore(FileStore):
         # Return path after copy
         return save_url
 
-    def get_file(self, filename: Union[str, Path]) -> str:
-        # Resolve path
-        filename = Path(filename).resolve().name
+    def download_file(self, filename: str, save_path: Optional[Union[str, Path]] = None, **kwargs) -> Path:
+        # Fix name
+        filename = Path(filename).resolve()
 
-        # Create blob
-        blob = self.bucket.blob(filename)
+        # Check for existance
+        self.get_file_uri(filename.name)
 
-        # Check if file exists
-        if blob.exists():
-            return blob.path
-        else:
-            raise FileNotFoundError(filename)
+        # No save path, set it to received filename
+        if save_path is None:
+            save_path = filename
+
+        # Check save path
+        save_path = Path(save_path).resolve()
+        if save_path.is_file():
+            raise FileExistsError(save_path)
+
+        # Begin download
+        log.debug(f"Beginning file download for: {filename}")
+        blob = self.bucket.blob(filename.name)
+        blob.download_to_filename(str(save_path))
+        log.debug(f"Completed file download for: {filename}")
+
+        return save_path

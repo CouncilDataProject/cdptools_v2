3fdcad75fd3a6120b38c93d6d1bdbbb9f5f8fe59
Jackson Brown <jacksonb@alleninstitute.org>
2019-04-18 00:59:34 -0700

    Resolves [gh-19], allow users to query by passing project id

--- cdptools/databases/cloud_firestore_database.py
+++ cdptools/databases/cloud_firestore_database.py
@@ -4,13 +4,16 @@
 from datetime import datetime
 import logging
 from pathlib import Path
-from typing import Dict, Optional, Union
+from typing import Dict, List, Optional, Tuple, Union
+from uuid import uuid4
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import requests
 
-from .database import Database
+from .database import Database, OrderCondition, WhereCondition
+from . import errors
 
 ###############################################################################
 
@@ -20,115 +23,293 @@ logging.basicConfig(
 )
 log = logging.getLogger(__file__)
 
+FIRESTORE_URI = "https://firestore.googleapis.com/v1/projects/{project_id}/databases/(default)/documents"
+
 ###############################################################################
+
+
+class WhereOperators:
+    eq: str = "=="
+    contains: str = "array_contains"
+    gt: str = ">"
+    lt: str = "<"
+    gteq: str = ">="
+    lteq: str = "<="
+
+
+class OrderOperators:
+    desc: str = "DESCENDING"
+    asce: str = "ASCENDING"
+
+
+class NoCredResponseTypes:
+    boolean: str = "booleanValue"
+    double: str = "doubleValue"
+    dt: str = "timestampValue"
+    integer: str = "integerValue"
+    null: str = "nullValue"
+    string: str = "stringValue"
 
 
 class CloudFirestoreDatabase(Database):
 
-    def __init__(self, credentials_path: Union[str, Path], name: Optional[str] = None, **kwargs):
-        # Resolve credentials
-        credentials_path = Path(credentials_path).resolve(strict=True)
+    def __init__(
+        self,
+        project_id: Optional[str] = None,
+        credentials_path: Optional[Union[str, Path]] = None,
+        name: Optional[str] = None,
+        **kwargs
+    ):
+        # With credentials:
+        if credentials_path:
+            # Resolve credentials
+            credentials_path = Path(credentials_path).resolve(strict=True)
 
-        # Initialize database reference
-        cred = credentials.Certificate(str(credentials_path))
+            # Initialize database reference
+            cred = credentials.Certificate(str(credentials_path))
 
-        # Check name
-        # This is done as if the name is None we just want to initialize the main connection
-        if name:
-            firebase_admin.initialize_app(cred, name=name)
+            # Check name
+            # This is done as if the name is None we just want to initialize the main connection
+            if name:
+                firebase_admin.initialize_app(cred, name=name)
+            else:
+                firebase_admin.initialize_app(cred)
+
+            # Store configuration
+            self._credentials_path = credentials_path
+            self._root = firestore.client()
+        elif project_id:
+            self._credentials_path = None
+            self._project_id = project_id
+            self._db_uri = FIRESTORE_URI.format(project_id=project_id)
         else:
-            firebase_admin.initialize_app(cred)
+            raise errors.MissingParameterError(["project_id", "credentials_path"])
 
-        # Store configuration
-        self.credentials_path = credentials_path
-        self.root = firestore.client()
+    @staticmethod
+    def _jsonify_firestore_response(fields: Dict) -> Dict:
+        formatted = {}
 
-    def get_event(self, event_id: str) -> Dict:
-        # Construct ref
-        ref = self.root.collection(u"events").document(event_id)
-        log.debug(f"Created reference: {ref}")
+        # Cast or parse values from returned
+        for k, type_and_value in fields.items():
+            if NoCredResponseTypes.boolean in type_and_value:
+                formatted[k] = type_and_value[NoCredResponseTypes.boolean]
+            elif NoCredResponseTypes.null in type_and_value:
+                formatted[k] = type_and_value[NoCredResponseTypes.null]
+            elif NoCredResponseTypes.string in type_and_value:
+                formatted[k] = type_and_value[NoCredResponseTypes.string]
+            elif NoCredResponseTypes.double in type_and_value:
+                formatted[k] = float(type_and_value[NoCredResponseTypes.double])
+            elif NoCredResponseTypes.integer in type_and_value:
+                formatted[k] = int(type_and_value[NoCredResponseTypes.integer])
+            elif NoCredResponseTypes.dt in type_and_value:
+                formatted[k] = datetime.strptime(
+                    type_and_value[NoCredResponseTypes.dt],
+                    "%Y-%m-%dT%H:%M:%S.%fZ"
+                )
 
-        # Attempt get
-        return ref.get().to_dict()
+        return formatted
 
-    def upload_event(self, event_details: Dict) -> str:
-        # Get key
-        key = event_details.pop("key")
+    def get_row(self, table: str, id: str) -> Dict:
+        # With credentials
+        if self._credentials_path:
+            # Get result
+            result = self._root.collection(table).document(id).get().to_dict()
 
-        # Check for exists
-        if self.get_event(key):
-            raise KeyError(f"An event with key {key} already exists.")
+            # Found, return expansion
+            if result:
+                return {"id": id, **result}
 
-        # Construct ref
-        ref = self.root.collection(u"events").document(key)
-        log.debug(f"Created reference: {ref}")
+            # Not found, return None
+            return None
 
-        # Add created timestamp
-        event_details[u"created_datetime"] = firestore.SERVER_TIMESTAMP
+        # Fill target uri
+        target_uri = f"{self._db_uri}/{table}/{id}"
+        response = requests.get(target_uri).json()
 
-        # Attempt set
-        return ref.set(event_details)
+        # Check for error
+        if "fields" in response:
+            # Format response
+            return {"id": id, **self._jsonify_firestore_response(response["fields"])}
 
-    def upload_error(self, error: Dict) -> str:
-        # Create error key
-        # Just use the current datetime
-        current_dt = str(datetime.utcnow()).replace(" ", "T")
+        raise KeyError(f"No row with id: {id} exists.")
 
-        # Construct ref
-        ref = self.root.collection(u"errors").document(current_dt)
-        log.debug(f"Created reference: {ref}")
+    @staticmethod
+    def _construct_where_condition(filt: Union[WhereCondition, List, Tuple]):
+        if isinstance(filt, WhereCondition):
+            return filt
+        elif isinstance(filt, (list, tuple)):
+            # Assume equal
+            if len(filt) == 2:
+                return WhereCondition(filt[0], WhereOperators.eq, filt[1])
+            elif len(filt) == 3:
+                return WhereCondition(*filt)
+            else:
+                raise errors.UnstructuredWhereConditionError(filt)
+        else:
+            raise errors.UnknownTypeWhereConditionError(filt)
 
-        # Attempt set
-        return ref.set(error)
+    @staticmethod
+    def _construct_orderby_condition(by: Union[List, OrderCondition, str, Tuple]):
+        if isinstance(by, OrderCondition):
+            return by
+        if isinstance(by, str):
+            # Assume descending
+            return OrderCondition(by, OrderOperators.desc)
+        elif isinstance(by, (list, tuple)):
+            # Assume descending
+            if len(by) == 1:
+                return OrderCondition(by[0], OrderOperators.desc)
+            elif len(by) == 2:
+                return OrderCondition(*by)
+            else:
+                raise errors.UnstructuredOrderConditionError(by)
+        else:
+            raise errors.UnknownTypeOrderConditionError(by)
 
-    def get_indexed_words(self) -> Dict[str, Dict]:
-        """
-        Get all preprocessed words.
-        """
+    def get_rows(
+        self,
+        table: str,
+        filters: Optional[List[Union[WhereCondition, List, Tuple]]] = None,
+        order_by: Optional[Union[List, OrderCondition, str, Tuple]] = None,
+        limit: Optional[int] = None
+    ) -> List[Dict]:
+        # With credentials
+        if self._credentials_path:
+            # Create base table ref
+            ref = self._root.collection(table)
 
-        return {}
+            # Apply filters
+            if filters:
+                for f in filters:
+                    # Construct WhereCondition
+                    f = self._construct_where_condition(f)
+                    # Apply
+                    ref = ref.where(f.column_name, f.operator, f.value)
 
-    def upload_indexed_words(self, words: Dict[str, Dict]) -> Dict[str, Dict]:
-        """
-        Provided a dictionary of dictionaries with {word: {indexing results}} update or add index results for each one.
-        """
+            # Apply order by
+            if order_by:
+                order_by = self._construct_orderby_condition(order_by)
+                ref = ref.order_by(order_by.column_name, order_by.operator)
 
-        return {}
+            # Apply limit
+            if limit:
+                ref = ref.limit(limit)
 
-    def get_transcript(self, transcript_id: str) -> str:
-        """
-        Get a transcript from the transcript id.
-        """
-        # Construct ref
-        ref = self.root.collection(u"transcripts").document(transcript_id)
-        log.debug(f"Created reference: {ref}")
+            # Get and expand
+            return [{"id": i.id, **i.to_dict()} for i in ref.stream()]
 
-        # Attempt get
-        return ref.get().to_dict()
+        target_uri = f"{self._db_uri}/{table}"
+        response = requests.get(target_uri).json()
 
-    def upload_transcript(self, transcript_details: Dict) -> str:
-        """
-        Upload a transcript and return the transcript id.
-        """
-        # Get key
-        key = transcript_details.pop("key")
+        # Check for error
+        if "documents" in response:
+            return [
+                {
+                    "id": d["name"].split("/")[-1],  # Get last item in the uri
+                    **self._jsonify_firestore_response(d["fields"])
+                } for d in response["documents"]
+            ]
+            return [{"id": d["name"], **self._jsonify_firestore_response(d["fields"])} for d in response["documents"]]
 
-        # Check for exists
-        if self.get_transcript(key):
-            raise KeyError(f"A transcript with key {key} already exists.")
+        raise KeyError(f"No table with name: {table} exits.")
 
-        # Construct ref
-        ref = self.root.collection(u"transcripts").document(key)
-        log.debug(f"Created reference: {ref}")
+    def _get_rows_with_max_results_expectation(
+        self,
+        table: str,
+        pks: List[Union[WhereCondition, List, Tuple]],
+        expected_max_rows: int
+    ):
+        # Find matching
+        pks = [self._construct_where_condition(pk) for pk in pks]
+        matching = self.get_rows(table=table, filters=pks)
 
-        # Add created timestamp
-        transcript_details[u"created_datetime"] = firestore.SERVER_TIMESTAMP
+        # Handle expectation
+        if len(matching) > expected_max_rows:
+            raise errors.UniquenessError(table, [pk.column_name for pk in pks], matching)
+        elif len(matching) == 0:
+            return None
+        else:
+            return matching[0]
 
-        # Attempt set
-        return ref.set(transcript_details)
+    def _get_or_upload_row(self, table: str, pks: List[Union[WhereCondition, List, Tuple]], values: Dict) -> Dict:
+        # Reject any upload without credentials
+        if self._credentials_path is None:
+            raise errors.MissingCredentialsError()
+
+        # Fast return for already stored
+        found = self._get_rows_with_max_results_expectation(
+            table=table,
+            pks=pks,
+            expected_max_rows=1
+        )
+        # Return or upload
+        if found:
+            return found
+        else:
+            # Create id
+            id = str(uuid4())
+            # Store the row
+            self._root.collection(table).document(id).set(values)
+
+            # Return row
+            return {"id": id, **values}
+
+    def get_or_upload_body(self, name: str, description: Optional[str] = None) -> Dict:
+        return self._get_or_upload_row(
+            table="body",
+            pks=[("name", name)],
+            values={
+                "name": name,
+                "description": description,
+                "created": datetime.utcnow()
+            }
+        )
+
+    def get_or_upload_event(
+        self,
+        body_id: int,
+        event_datetime: datetime,
+        source_uri: str,
+        thumbnail_uri: str,
+        video_uri: str
+    ) -> Dict:
+        return self._get_or_upload_row(
+            table="event",
+            pks=[("video_uri", video_uri)],
+            values={
+                "body_id": body_id,
+                "event_datetime": event_datetime,
+                "source_uri": source_uri,
+                "thumbnail_uri": thumbnail_uri,
+                "video_uri": video_uri,
+                "created": datetime.utcnow()
+            }
+        )
+
+    def get_or_upload_algorithm(
+        self,
+        name: str,
+        version: str,
+        description: Optional[str] = None,
+        source: Optional[str] = None
+    ) -> Dict:
+        return self._get_or_upload_row(
+            table="algorithm",
+            pks=[("name", name), ("version", version)],
+            values={
+                "name": name,
+                "version": version,
+                "description": description,
+                "source": source,
+                "created": datetime.utcnow()
+            }
+        )
 
     def __str__(self):
-        return f"<FirebaseDatabase [{self.credentials_path}]>"
+        if self._credentials_path:
+            return f"<FirebaseDatabase [{self._credentials_path}]>"
+
+        return f"<FirebaseDatabase [{self._project_id}]"
 
     def __repr__(self):
         return str(self)
---
+++ cdptools/databases/errors.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from typing import Dict, List, Tuple, Union
+
+
+class MissingParameterError(Exception):
+    def __init__(self, parameter_options: List[str], **kwargs):
+        super().__init__(**kwargs)
+        self.parameter_options = parameter_options
+
+    def __str__(self):
+        return f"Missing at least one parameter from options: {self.parameter_options}"
+
+
+class UnstructuredWhereConditionError(Exception):
+    def __init__(self, filt: Union[List, Tuple], **kwargs):
+        super().__init__(**kwargs)
+        self.filt = filt
+
+    def __str__(self):
+        return f"WhereCondition's accept at least 2 and at most 3 parameters. Received: {self.filt}"
+
+
+class UnknownTypeWhereConditionError(Exception):
+    def __init__(self, filt: object, **kwargs):
+        super().__init__(**kwargs)
+        self.filt = filt
+
+    def __str__(self):
+        return f"WhereCondition's can be created using list or tuple. Received: {type(self.filt)}, {self.filt}"
+
+
+class UnstructuredOrderConditionError(Exception):
+    def __init__(self, by: Union[List, Tuple], **kwargs):
+        super().__init__(**kwargs)
+        self.by = by
+
+    def __str__(self):
+        return f"OrderCondition's accept at least 1 and at most 2 parameters. Received: {self.by}"
+
+
+class UnknownTypeOrderConditionError(Exception):
+    def __init__(self, by: object, **kwargs):
+        super().__init__(**kwargs)
+        self.by = by
+
+    def __str__(self):
+        return f"OrderCondition's can be created using list, string, or tuple. Received: {type(self.by)}, {self.by}"
+
+
+class UniquenessError(Exception):
+    def __init__(self, table: str, pks: List[str], results: List[Dict], **kwargs):
+        super().__init__(**kwargs)
+        self.table = table
+        self.pks = pks
+        self.results = results
+
+    def __str__(self):
+        return f"Table: {self.table}, primary keys: {self.pks} not upheld.\nQuery returned: {self.results}"
+
+
+class MissingCredentialsError(Exception):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def __str__(self):
+        return "Operation not permitted without providing credentials."

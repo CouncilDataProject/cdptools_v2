8b4fb6d26d39387272511a9cf783535f9165a3c2
Jackson Brown <jacksonb@alleninstitute.org>
2019-04-27 11:09:38 -0700

    Resolves [gh-30], build procedure updates

--- setup.py
+++ setup.py
@@ -11,27 +11,16 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [
-    'beautifulsoup4>=4.7.1',
-    'ffmpeg-python>=0.1.17',
-    'fuzzywuzzy>=0.17.0',
-    'pandas',
-    'requests>=2.21.0',
-    'schedule>=0.6.0'
+test_requirements = [
+    'pytest',
+    'pytest-cov',
+    'pytest-raises',
+    'flake8',
 ]
 
-local_requires = [
-    'appdirs>=1.4.3'
-]
+setup_requirements = ['pytest-runner', ]
 
-google_cloud_requires = [
-    'firebase-admin>=2.16.0',
-    'google-cloud-speech>=1.0.0',
-    'google-cloud-storage>=1.14.0'
-]
-
-dev_requires = [
-    'pip>=19.0.3',
+dev_requirements = [
     'bumpversion>=0.5.3',
     'wheel>=0.33.1',
     'flake8>=3.7.7',
@@ -45,22 +34,47 @@ dev_requires = [
     'pytest-runner>=4.4',
 ]
 
-setup_requirements = [
-    'pytest-runner>=4.4',
+interactive_requirements = [
+    'altair',
+    'jupyterlab',
+    'matplotlib',
 ]
 
-test_requirements = [
-    'pytest>=4.3.0',
-    'pytest-cov==2.6.1',
-    'pytest-raises>=0.10',
+requirements = [
+    'beautifulsoup4>=4.7.1',
+    'ffmpeg-python>=0.1.17',
+    'fuzzywuzzy>=0.17.0',
+    'pandas',
+    'requests>=2.21.0',
+    'schedule>=0.6.0'
+]
+
+local_requirements = [
+    'appdirs>=1.4.3'
+]
+
+google_cloud_requirements = [
+    'firebase-admin>=2.16.0',
+    'google-cloud-speech>=1.0.0',
+    'google-cloud-storage>=1.14.0'
 ]
 
 extra_requirements = {
     'test': test_requirements,
     'setup': setup_requirements,
-    'dev': dev_requires,
-    'local': local_requires,
-    'google-cloud': google_cloud_requires
+    'dev': dev_requirements,
+    'interactive': interactive_requirements,
+    'local': local_requirements,
+    'google-cloud': google_cloud_requirements,
+    'all': [
+        *requirements,
+        *local_requirements,
+        *google_cloud_requirements,
+        *test_requirements,
+        *setup_requirements,
+        *dev_requirements,
+        *interactive_requirements
+    ]
 }
 
 setup(
@@ -84,7 +98,7 @@ setup(
     license="MIT license",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
-    keywords='Council Data Project Tools',
+    keywords='Council Data Project Pipeline Tools',
     name='cdptools',
     packages=find_packages(include=['cdptools']),
     setup_requires=setup_requirements,

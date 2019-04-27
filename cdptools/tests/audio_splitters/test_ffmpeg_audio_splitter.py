b632b29fea8fa3df645272585b01d1aa42cec61a
Jackson Brown <jacksonb@alleninstitute.org>
2019-04-27 10:49:04 -0700

    Fixes FFmpeg audio splitter tests.

--- cdptools/tests/audio_splitters/test_ffmpeg_audio_splitter.py
+++ cdptools/tests/audio_splitters/test_ffmpeg_audio_splitter.py
@@ -8,19 +8,11 @@ from unittest import mock
 from cdptools.audio_splitters.ffmpeg_audio_splitter import FFmpegAudioSplitter
 
 
-@pytest.fixture(scope="module", autouse=True)
+@pytest.fixture
 def example_video(data_dir) -> Path:
     return data_dir / "example.mp4"
 
-# When this fixture is passed, it will catch requests for ffmpeg.run
-@pytest.fixture(scope="module", autouse=True)
-def mocked_ffmpeg():
-    with mock.patch("ffmpeg") as MockFFmpeg:
-        MockFFmpeg.run.return_value = ("hello", "world")
-        yield MockFFmpeg
 
-
-@pytest.fixture(autouse=True)
 def test_real_audio_split(tmpdir, example_video):
     # Initialize splitter
     splitter = FFmpegAudioSplitter()
@@ -32,17 +24,21 @@ def test_real_audio_split(tmpdir, example_video):
     splitter.split(video_read_path=example_video, audio_save_path=fake_save_path)
 
 
-@pytest.fixture(autouse=True)
 @pytest.mark.parametrize("audio_save_path", [
-    ("~/this/is/a/test.wav"),
-    (Path("~/this/is/a/test.wav")),
+    ("test.wav"),
+    (Path("test.wav")),
     pytest.param(__file__, marks=pytest.mark.raises(exception=FileExistsError)),
     pytest.param(Path(__file__), marks=pytest.mark.raises(exception=FileExistsError)),
     pytest.param(Path(__file__).parent, marks=pytest.mark.raises(exception=IsADirectoryError))
 ])
-def test_mocked_save_path(example_video, audio_save_path, mocked_ffmpeg):
+def test_mocked_save_path(tmpdir, example_video, audio_save_path):
+    # Append save name to tmpdir
+    audio_save_path = Path(tmpdir) / audio_save_path
+
     # Initialize splitter
     splitter = FFmpegAudioSplitter()
 
     # Mock split
-    splitter.split(video_read_path=example_video, audio_save_path=audio_save_path)
+    with mock.patch("ffmpeg.run") as mocked_ffmpeg:
+        mocked_ffmpeg.return_value = (b"OUTPUT", b"ERROR")
+        splitter.split(video_read_path=example_video, audio_save_path=audio_save_path)
--- cdptools/tests/conftest.py
+++ cdptools/tests/conftest.py
@@ -6,6 +6,6 @@ from pathlib import Path
 import pytest
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def data_dir() -> Path:
     return Path(__file__).parent / "data"

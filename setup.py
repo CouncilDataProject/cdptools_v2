8fd77dc88ba7580ef7cf104c09810aa43691a65b
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2019-06-09 23:41:32 -0700

    Fix setup.py after removal of extra cookiecutter files

--- setup.py
+++ setup.py
@@ -8,9 +8,6 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 test_requirements = [
     'codecov',
     'flake8',
@@ -99,7 +96,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='Council Data Project Pipeline Tools',
     name='cdptools',

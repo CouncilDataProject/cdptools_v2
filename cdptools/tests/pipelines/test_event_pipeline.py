849deef69205446f2a8150b7083f58111fec7a2a
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2019-06-14 16:13:18 -0700

    Add tests and fixtures required for basic testing of event pipeline

--- cdptools/tests/pipelines/test_event_pipeline.py
+++ cdptools/tests/pipelines/test_event_pipeline.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
 from pathlib import Path
 import pytest
+from typing import Union
 from unittest import mock
 
 from firebase_admin import firestore
@@ -78,18 +80,68 @@ def mocked_sr_model(
     mocked_model = mock.Mock(GoogleCloudSRModel(example_transcript_raw))
     mocked_model.transcribe.return_value = SRModelOutputs(
         example_transcript_raw,
-        12.0,
+        99.0,
         example_transcript_words,
         example_transcript_sentences
     )
 
+    return mocked_model
 
-def test_event_pipeline(
+
+@pytest.fixture
+def example_seattle_routes(data_dir):
+    return data_dir / "example_seattle_routes.html"
+
+
+@pytest.fixture
+def example_seattle_route(data_dir):
+    return data_dir / "example_seattle_route.html"
+
+
+@pytest.fixture
+def example_legistar_tools_events(data_dir):
+    return data_dir / "example_legistar_tools_events.json"
+
+
+@pytest.fixture
+def example_legistar_tools_event_items_0(data_dir):
+    return data_dir / "example_legistar_tools_event_items_0.json"
+
+
+@pytest.fixture
+def example_legistar_tools_event_items_1(data_dir):
+    return data_dir / "example_legistar_tools_event_items_1.json"
+
+
+@pytest.fixture
+def example_legistar_tools_event_item_vote(data_dir):
+    return data_dir / "example_legistar_tools_event_item_vote.json"
+
+
+class RequestReturn:
+    def __init__(self, content: Union[str, Path]):
+        if isinstance(content, Path):
+            with open(content, "r") as read_in:
+                if content.suffix == ".json":
+                    content = json.load(read_in)
+                else:
+                    content = read_in.read()
+
+        self.content = content
+
+    def raise_for_status(self):
+        pass
+
+    def json(self):
+        return self.content
+
+
+def test_event_pipeline_no_backfill(
     empty_creds_db,
     empty_creds_fs,
     mocked_sr_model,
     example_config,
-    example_video
+    example_seattle_routes
 ):
     # Configure all mocks
     with mock.patch("cdptools.pipelines.pipeline.Pipeline.load_custom_object") as mock_loader:
@@ -98,10 +150,56 @@ def test_event_pipeline(
         ]
 
         # Initialize pipeline
+        pipeline = mock.Mock(EventPipeline(example_config))
+
+        with mock.patch("requests.get") as mock_requests:
+            # No backfill means only routes will be gathered because example html file only includes past events.
+            mock_requests.side_effect = [RequestReturn(example_seattle_routes)]
+
+            pipeline.run()
+
+            # This should never be ran because example html files only include past events.
+            pipeline.process_event.assert_not_called()
+
+# TODO:
+# The test returns the wrong legistar event details for the provided seattle channel event
+
+def test_event_pipeline_with_backfill(
+    empty_creds_db,
+    empty_creds_fs,
+    mocked_sr_model,
+    example_config,
+    example_seattle_routes,
+    example_seattle_route,
+    example_legistar_tools_events,
+    example_legistar_tools_event_items_0,
+    example_legistar_tools_event_items_1,
+    example_legistar_tools_event_item_vote,
+    example_video
+):
+    # Configure all mocks
+    with mock.patch("cdptools.pipelines.pipeline.Pipeline.load_custom_object") as mock_loader:
+        mock_loader.side_effect = [
+            SeattleEventScraper(backfill=True), empty_creds_db, empty_creds_fs, FFmpegAudioSplitter(), mocked_sr_model
+        ]
+
+        # Initialize pipeline
         pipeline = EventPipeline(example_config)
 
-        print(pipeline.event_scraper)
-        print(pipeline.database)
-        print(pipeline.file_store)
-        print(pipeline.audio_splitter)
-        print(pipeline.sr_model)
+        # Pre read event item returns
+        event_items_zero = RequestReturn(example_legistar_tools_event_items_0)
+        event_items_one = RequestReturn(example_legistar_tools_event_items_1)
+
+        with mock.patch("requests.get") as mock_requests:
+            # Back means we need to mock every request call including all the legistar calls
+            mock_requests.side_effect = [
+                RequestReturn(example_seattle_routes),
+                RequestReturn(example_seattle_route),
+                RequestReturn(example_legistar_tools_events),
+                event_items_zero,
+                *([RequestReturn(example_legistar_tools_event_item_vote)] * len(event_items_zero.content)),
+                event_items_one,
+                *([RequestReturn(example_legistar_tools_event_item_vote)] * len(event_items_one.content))
+            ]
+
+            pipeline.run()

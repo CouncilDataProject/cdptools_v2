656bfc0636a66ef15a76cb08a5d8ed926e5835a8
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2019-07-07 21:00:57 -0700

    Remove test cdp pipeline bin script as there is now somewhat of a test

--- setup.py
+++ setup.py
@@ -3,69 +3,69 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
 test_requirements = [
-    'codecov',
-    'flake8',
-    'pytest',
-    'pytest-cov',
-    'pytest-raises',
+    "codecov",
+    "flake8",
+    "pytest",
+    "pytest-cov",
+    "pytest-raises",
 ]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ["pytest-runner", ]
 
 dev_requirements = [
-    'bumpversion>=0.5.3',
-    'wheel>=0.33.1',
-    'flake8>=3.7.7',
-    'tox>=3.5.2',
-    'coverage>=5.0a4',
-    'Sphinx>=2.0.0b1',
-    'twine>=1.13.0',
-    'pytest>=4.3.0',
-    'pytest-cov==2.6.1',
-    'pytest-raises>=0.10',
-    'pytest-runner>=4.4',
+    "bumpversion>=0.5.3",
+    "wheel>=0.33.1",
+    "flake8>=3.7.7",
+    "tox>=3.5.2",
+    "coverage>=5.0a4",
+    "Sphinx>=2.0.0b1",
+    "twine>=1.13.0",
+    "pytest>=4.3.0",
+    "pytest-cov==2.6.1",
+    "pytest-raises>=0.10",
+    "pytest-runner>=4.4",
 ]
 
 interactive_requirements = [
-    'altair',
-    'jupyterlab',
-    'matplotlib',
+    "altair",
+    "jupyterlab",
+    "matplotlib",
 ]
 
 requirements = [
-    'beautifulsoup4>=4.7.1',
-    'ffmpeg-python>=0.1.17',
-    'fuzzywuzzy>=0.17.0',
-    'pandas',
-    'python-Levenshtein>=0.12.0',
-    'requests>=2.21.0',
-    'schedule>=0.6.0'
+    "beautifulsoup4>=4.7.1",
+    "ffmpeg-python>=0.1.17",
+    "fuzzywuzzy>=0.17.0",
+    "pandas",
+    "python-Levenshtein>=0.12.0",
+    "requests>=2.21.0",
+    "schedule>=0.6.0"
 ]
 
 local_requirements = [
-    'appdirs>=1.4.3'
+    "appdirs>=1.4.3"
 ]
 
 google_cloud_requirements = [
-    'firebase-admin>=2.16.0',
-    'google-cloud-speech>=1.0.0',
-    'google-cloud-storage>=1.14.0'
+    "firebase-admin>=2.16.0",
+    "google-cloud-speech>=1.0.0",
+    "google-cloud-storage>=1.14.0"
 ]
 
 extra_requirements = {
-    'test': test_requirements,
-    'setup': setup_requirements,
-    'dev': dev_requirements,
-    'interactive': interactive_requirements,
-    'local': local_requirements,
-    'google-cloud': google_cloud_requirements,
-    'all': [
+    "test": test_requirements,
+    "setup": setup_requirements,
+    "dev": dev_requirements,
+    "interactive": interactive_requirements,
+    "local": local_requirements,
+    "google-cloud": google_cloud_requirements,
+    "all": [
         *requirements,
         *local_requirements,
         *google_cloud_requirements,
@@ -78,34 +78,33 @@ extra_requirements = {
 
 setup(
     author="Jackson Maxfield Brown",
-    author_email='jmaxfieldbrown@gmail.com',
+    author_email="jmaxfieldbrown@gmail.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="Tools used to run Council Data Project pipelines.",
     entry_points={
-        'console_scripts': [
-            'run_cdp_pipeline=cdptools.bin.run_cdp_pipeline:main',
-            'test_cdp_pipeline=cdptools.bin.test_cdp_pipeline:main',
+        "console_scripts": [
+            "run_cdp_pipeline=cdptools.bin.run_cdp_pipeline:main",
         ],
     },
     install_requires=requirements,
     license="MIT license",
     long_description=readme,
     include_package_data=True,
-    keywords='Council Data Project Pipeline Tools',
-    name='cdptools',
-    packages=find_packages(exclude=['tests']),
+    keywords="Council Data Project Pipeline Tools",
+    name="cdptools",
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
     extras_require=extra_requirements,
-    url='https://github.com/JacksonMaxfield/cdptools',
-    version='0.1.0',
+    url="https://github.com/JacksonMaxfield/cdptools",
+    version="0.1.0",
     zip_safe=False,
 )

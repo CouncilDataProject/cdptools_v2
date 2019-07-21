f44299b1d7e4c1290b11e0b224b8ac528b1aecd4
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2019-07-21 11:06:40 -0700

    Add index pipeline

--- cdptools/pipelines/__init__.py
+++ cdptools/pipelines/__init__.py
@@ -2,5 +2,6 @@
 
 """Pipelines package for cdptools."""
 
-from .pipeline import Pipeline  # noqa: F401
 from .event_pipeline import EventPipeline  # noqa: F401
+from .index_pipeline import IndexPipeline  # noqa: F401
+from .pipeline import Pipeline  # noqa: F401

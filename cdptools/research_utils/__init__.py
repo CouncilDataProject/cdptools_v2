e36a148dd612a7b8628b518a8af876297c127646
Jackson Brown <jmaxfieldbrown@gmail.com>
2019-07-25 15:41:01 -0700

    Add test for running index pipeline

--- cdptools/research_utils/__init__.py
+++ cdptools/research_utils/__init__.py
@@ -2,4 +2,5 @@
 
 """Research utilities package for cdptools."""
 
-from .transcripts import download_most_recent_transcripts  # noqa: F401
+from .transcripts import (download_most_recent_transcripts,  # noqa: F401
+                          get_most_recent_transcript_manifest)

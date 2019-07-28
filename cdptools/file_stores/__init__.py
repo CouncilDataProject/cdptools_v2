690861154418d6fa0dc08e27ebb6a22b26e1b0f2
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2019-07-28 10:44:35 -0700

    Expose certain objects rather than having to follow entire module path

--- cdptools/file_stores/__init__.py
+++ cdptools/file_stores/__init__.py
@@ -3,4 +3,6 @@
 """File stores package for cdptools."""
 
 
+from .app_dirs_file_store import AppDirsFileStore  # noqa: F401
 from .file_store import FileStore  # noqa: F401
+from .gcs_file_store import GCSFileStore  # noqa: F401

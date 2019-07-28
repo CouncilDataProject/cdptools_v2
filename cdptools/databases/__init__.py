bd9b67ccf955466eb208746abd2453c7e838979f
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2019-07-28 10:44:35 -0700

    Expose certain objects rather than having to follow entire module path

--- cdptools/databases/__init__.py
+++ cdptools/databases/__init__.py
@@ -3,5 +3,6 @@
 """Databases package for cdptools."""
 
 
+from .cloud_firestore_database import CloudFirestoreDatabase  # noqa: F401
 from .database import (Database, OrderCondition, OrderOperators,  # noqa: F401
                        WhereCondition, WhereOperators)

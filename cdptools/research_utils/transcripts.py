b7df7fd4fd958e17f8f850ac25f70da9c228449a
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2019-07-28 11:41:16 -0700

    Docstrings for research utils transcripts

--- cdptools/research_utils/transcripts.py
+++ cdptools/research_utils/transcripts.py
@@ -8,6 +8,21 @@ from cdptools.file_stores import FileStore
 
 
 def get_most_recent_transcript_manifest(db: Database) -> pd.DataFrame:
+    """
+    Get a pandas dataframe that can act as a manifest of the most recent transcript available for each event stored in a
+    CDP instance's database.
+
+    Parameters
+    ----------
+    db: Database
+        An already initialized database object connected to a CDP instance's database.
+
+    Returns
+    -------
+    manifest: pandas.DataFrame
+        A dataframe with transcript, event, body, and file details where each row is the most recent transcript for the
+        event of that row.
+    """
     # Get transcript dataset
     transcripts = pd.DataFrame(db.select_rows_as_list("transcript"))
     events = pd.DataFrame(db.select_rows_as_list("event"))
@@ -33,10 +48,20 @@ def download_most_recent_transcripts(db: Database, fs: FileStore, save_dir: Opti
     """
     Download the most recent versions of event transcripts.
 
-    :param db: An already initialized `Database` object to query against.
-    :param fs: An alreay initialized `FileStore` object to query against.
-    :param save_dir: Path to a directory to save the transcripts and the dataset manifest.
-    :return: Fully resolved path where transcripts and dataset manifest were stored.
+    Parameters
+    ----------
+    db: Database
+        An already initialized database object connected to a CDP instance's database.
+    fs: FileStore
+        An already initialized file store object connected to a CDP instance's file store.
+    save_dir: Optional[Union[str, Path]]
+        An optional path of where to save the transcripts and manifest CSV. If None provided, uses current directory.
+        Always overwrites existing transcripts with the same name if they already exist in the provided directory.
+
+    Returns
+    -------
+    event_corpus_map: Dict[str, Path]
+        A dictionary mapping event id to local Path of the most recent transcript for that event.
     """
     # Use current directory is None provided
     if save_dir is None:

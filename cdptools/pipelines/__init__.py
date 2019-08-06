d80968b38120a0f60cd7b202abf608ff44948824
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2019-08-05 21:23:55 -0700

    Add minutes item index pipeline, expose to init, and add config

--- cdptools/pipelines/__init__.py
+++ cdptools/pipelines/__init__.py
@@ -4,4 +4,5 @@
 
 from .event_gather_pipeline import EventGatherPipeline  # noqa: F401
 from .event_index_pipeline import EventIndexPipeline  # noqa: F401
+from .minutes_item_index_pipeline import MinutesItemIndexPipeline  # noqa: F401
 from .pipeline import Pipeline  # noqa: F401

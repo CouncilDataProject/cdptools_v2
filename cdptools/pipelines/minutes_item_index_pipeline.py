1e8b5dca6776f3610f826a1af28ff6228e350b7a
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2019-08-05 23:22:05 -0700

    Wrap MinutesItemIndexPipeline generate corpus in a RunManager

--- cdptools/pipelines/minutes_item_index_pipeline.py
+++ cdptools/pipelines/minutes_item_index_pipeline.py
@@ -53,88 +53,98 @@ class MinutesItemIndexPipeline(Pipeline):
         )
 
     def task_generate_corpus(self, save_dir: Path) -> Dict[str, Path]:
-        # Read this stackoverflow for how we are selecting anything that isn't null
-        # https://stackoverflow.com/questions/48479532/firestore-select-where-is-not-null
-        decision_items = self.database.select_rows_as_list("event_minutes_item", filters=[("decision", ">", "")])
+        """
+        Generate the minutes item corpus (decision item corpus) to be indexed.
+        """
+        with RunManager(
+            database=self.database,
+            file_store=self.file_store,
+            algorithm_name="MinutesItemIndexPipeline.task_generate_corpus",
+            algorithm_version=get_module_version()
+        ):
+            # Get only minutes items that had a decision
+            # This will generally reduce the minutes items to just bills, appointments, etc
+            # Read this stackoverflow for how we are selecting anything that isn't null
+            # https://stackoverflow.com/questions/48479532/firestore-select-where-is-not-null
+            decision_items = self.database.select_rows_as_list("event_minutes_item", filters=[("decision", ">", "")])
 
-        # Get only minutes items that had a decision
-        # This will generally reduce the minutes items to just bills, appointments, etc
-        for i, di in enumerate(decision_items):
-            decision_items[i] = {**di, **self.database.select_row_by_id("minutes_item", di["minutes_item_id"])}
+            # Attach minutes item information to each decision item
+            for i, di in enumerate(decision_items):
+                decision_items[i] = {**di, **self.database.select_row_by_id("minutes_item", di["minutes_item_id"])}
 
-        # Gather event info and reformat object structure to be a dictionary mapping
-        # minutes item to list of events that minutes item was discussed in
-        di_to_events = {}
-        for di in decision_items:
-            if di["minutes_item_id"] in di_to_events:
-                di_to_events[di["minutes_item_id"]].append(self.database.select_row_by_id("event", di["event_id"]))
-            else:
-                di_to_events[di["minutes_item_id"]] = [self.database.select_row_by_id("event", di["event_id"])]
-
-        # Add list of most recent transcript filenames
-        di_to_transcripts = {}
-        for di, events in di_to_events.items():
-            for event in events:
-                transcript = self.database.select_rows_as_list(
-                    "transcript",
-                    filters=[("event_id", event["event_id"])],
-                    order_by=("created", "DESCENDING"),
-                    limit=1
-                )[0]
-                file_details = self.database.select_row_by_id("file", transcript["file_id"])
-
-                if di in di_to_transcripts:
-                    di_to_transcripts[di].append(file_details["filename"])
+            # Gather event info and reformat object structure to be a dictionary mapping
+            # minutes item to list of events that minutes item was discussed in
+            di_to_events = {}
+            for di in decision_items:
+                if di["minutes_item_id"] in di_to_events:
+                    di_to_events[di["minutes_item_id"]].append(self.database.select_row_by_id("event", di["event_id"]))
                 else:
-                    di_to_transcripts[di] = [file_details["filename"]]
+                    di_to_events[di["minutes_item_id"]] = [self.database.select_row_by_id("event", di["event_id"])]
 
-        # Download and merge transcripts to single file to act as the decision_item_corpus_map
-        di_corpus_map = {}
-        for di, files in di_to_transcripts.items():
-            # Collect the transcript texts into a fake transcript document
-            di_transcript_document = {
-                "format": "timestamped-sentences",
-                "annotations": [],
-                "confidence": None,
-                "data": []
-            }
+            # Add list of most recent transcript filenames
+            di_to_transcripts = {}
+            for di, events in di_to_events.items():
+                for event in events:
+                    transcript = self.database.select_rows_as_list(
+                        "transcript",
+                        filters=[("event_id", event["event_id"])],
+                        order_by=("created", "DESCENDING"),
+                        limit=1
+                    )[0]
+                    file_details = self.database.select_row_by_id("file", transcript["file_id"])
 
-            # Use the provided save directory to download each file, read,
-            # and add all data to the decision transcript document
-            for filename in files:
-                saved = self.file_store.download_file(filename, save_dir, overwrite=True)
-                with open(saved, "r") as read_in:
-                    transcript = json.load(read_in)
-                    di_transcript_document["data"] += transcript["data"]
+                    if di in di_to_transcripts:
+                        di_to_transcripts[di].append(file_details["filename"])
+                    else:
+                        di_to_transcripts[di] = [file_details["filename"]]
 
-            # Use the provided save directory to download each minutes item file tied to the decision item
-            # (usually the bill text, a presentation about the project or bill, or document about the appointment)
-            # Parse each file and add each file's contents to the transcript data
-            di_files = self.database.select_rows_as_list("minutes_item_file", filters=[("minutes_item_id", di)])
-            for di_f in di_files:
-                # In case the file no longer exists 😡
-                try:
-                    local_path = self.file_store._external_resource_copy(di_f["uri"], save_dir, overwrite=True)
-                    parsed = parser.from_file(str(local_path))
-                    # Sometimes the content can't be parsed 🤷
-                    if parsed["content"]:
-                        di_transcript_document["data"].append({
-                            "text": parsed["content"],
-                            "start_time": 0.0,
-                            "end_time": 1.0
-                        })
-                except requests.exceptions.HTTPError:
-                    pass
+            # Download and merge transcripts to single file to act as the decision_item_corpus_map
+            di_corpus_map = {}
+            for di, files in di_to_transcripts.items():
+                # Collect the transcript texts into a fake transcript document
+                di_transcript_document = {
+                    "format": "timestamped-sentences",
+                    "annotations": [],
+                    "confidence": None,
+                    "data": []
+                }
 
-            # Store the join of all transcripts in a single file for the corpus
-            document_path = save_dir / f"document_{di}.json"
-            with open(document_path, "w") as write_out:
-                json.dump(di_transcript_document, write_out)
+                # Use the provided save directory to download each file, read,
+                # and add all data to the decision transcript document
+                for filename in files:
+                    saved = self.file_store.download_file(filename, save_dir, overwrite=True)
+                    with open(saved, "r") as read_in:
+                        transcript = json.load(read_in)
+                        di_transcript_document["data"] += transcript["data"]
 
-            # Add the document path to the di corpus map
-            di_corpus_map[di] = document_path
+                # Use the provided save directory to download each minutes item file tied to the decision item
+                # (usually the bill text, a presentation about the project or bill, or document about the appointment)
+                # Parse each file and add each file's contents to the transcript data
+                di_files = self.database.select_rows_as_list("minutes_item_file", filters=[("minutes_item_id", di)])
+                for di_f in di_files:
+                    # In case the file no longer exists 😡
+                    try:
+                        local_path = self.file_store._external_resource_copy(di_f["uri"], save_dir, overwrite=True)
+                        parsed = parser.from_file(str(local_path))
+                        # Sometimes the content can't be parsed 🤷
+                        if parsed["content"]:
+                            di_transcript_document["data"].append({
+                                "text": parsed["content"],
+                                "start_time": 0.0,
+                                "end_time": 1.0
+                            })
+                    except requests.exceptions.HTTPError:
+                        pass
 
-        return di_corpus_map
+                # Store the join of all transcripts in a single file for the corpus
+                document_path = Path(save_dir) / f"document_{di}.json"
+                with open(document_path, "w") as write_out:
+                    json.dump(di_transcript_document, write_out)
+
+                # Add the document path to the di corpus map
+                di_corpus_map[di] = document_path
+
+            return di_corpus_map
 
     def task_generate_index(self, minutes_item_corpus_map: Dict[str, Path]) -> Dict[str, Dict[str, float]]:
         """

913b6dd0cdf3d75c0246b94c71e0748de9fdf0d1
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2019-08-07 16:39:23 -0700

    Bump version: 2.0.1 → 2.0.2

--- cdptools/__init__.py
+++ cdptools/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = "Jackson Maxfield Brown"
 __email__ = "jmaxfieldbrown@gmail.com"
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 from .cdp_instance import CDPInstance  # noqa: F401
 

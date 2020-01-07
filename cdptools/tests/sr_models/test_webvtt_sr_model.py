e6b70e2ea727ea8df99184f0029670448d581fc9
To Huynh <huynh.nto@gmail.com>
2020-01-07 15:06:57 -0800

    add tests for bugfix

--- cdptools/tests/sr_models/test_webvtt_sr_model.py
+++ cdptools/tests/sr_models/test_webvtt_sr_model.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import json
 from pathlib import Path
 from requests import RequestException
 
 from unittest.mock import Mock
+from webvtt.structures import Caption
 
 import pytest
 
@@ -15,11 +15,6 @@ from cdptools.sr_models.webvtt_sr_model import WebVTTSRModel
 @pytest.fixture
 def fake_caption(data_dir) -> Path:
     return data_dir / "fake_caption.vtt"
-
-
-@pytest.fixture
-def fake_timestamped_sentences(data_dir) -> Path:
-    return data_dir / "fake_timestamped_sentences.json"
 
 
 @pytest.fixture
@@ -34,26 +29,81 @@ def test_webvtt_sr_model_request_caption_content(example_webvtt_sr_model):
         example_webvtt_sr_model._request_caption_content("invalid-caption-uri")
 
 
-# Check whether timestamped sentences of fake_caption equals fake_timestamped_sentences
-def test_webvtt_sr_model_get_sentences(example_webvtt_sr_model, fake_caption, fake_timestamped_sentences):
+@pytest.mark.parametrize("captions, expected", [
+    (
+        [
+            Caption(text="&gt;&gt; Start of Dialog 1."),
+            Caption(text="End of Dialog 1."),
+            Caption(text="&gt;&gt; [ APPLAUSE ] "),
+            Caption(text="&gt;&gt; Dialog 2.")
+        ],
+        [2, 1, 1]
+    ),
+    (
+        [
+            Caption(text="&gt;&gt; Dialog 1."),
+            Caption(text="&gt;&gt; [ ROLL BEING CALLED ] "),
+            Caption(text="&gt;&gt; Dialog 2."),
+        ],
+        [1, 1, 1]
+    ),
+    (
+        [
+            Caption(text="&gt;&gt; [ LAUGHTER ] Dialog 1."),
+            Caption(text="&gt;&gt; [ APPLAUSE ]"),
+            Caption(text="&gt;&gt; Dialog 2.")
+        ],
+        [1, 1, 1]
+    ),
+    (
+        [
+            Caption(text="&gt;&gt; Sentence"),
+            Caption(text="one."),
+            Caption(text="&gt;&gt; Sentence"),
+            Caption(text="two!"),
+            Caption(text="Sentence"),
+            Caption(text="three!"),
+            Caption(text="Sentence"),
+            Caption(text="four?")
+        ],
+        [1, 3]
+    ),
+    (
+        [
+            Caption(text="&gt;&gt; Sentence"),
+            Caption(text="one, no sentence ending punctuation"),
+            Caption(text="&gt;&gt; Sentence"),
+            Caption(text="two.")
+        ],
+        [1, 1]
+    ),
+    (
+        [
+            Caption(text="Sentence one."),
+            Caption(text="ú&gt;&gt; Sentence two.")
+        ],
+        [1, 1]
+    )
+])
+def test_webvtt_sr_model_create_timestamped_speaker_turns(
+    captions,
+    expected,
+    example_webvtt_sr_model
+):
+    speaker_turns = example_webvtt_sr_model._get_speaker_turns(captions)
+    timestamped_speaker_turns = example_webvtt_sr_model._create_timestamped_speaker_turns(speaker_turns)
+    # Check if the number of speaker turns is correct
+    assert len(timestamped_speaker_turns) == len(expected)
+    # Check if the number of sentences per speaker turn is correct
+    for i, number_of_sentences in enumerate(expected):
+        assert len(timestamped_speaker_turns[i]["data"]) == number_of_sentences
+
+
+def test_webvtt_sr_model_transcribe(example_webvtt_sr_model, fake_caption, tmpdir):
     with open(fake_caption, "r") as fake_caption_file:
         caption_text = fake_caption_file.read()
 
     example_webvtt_sr_model._request_caption_content = Mock(return_value=caption_text)
-
-    with open(fake_timestamped_sentences) as json_file:
-        timestamped_sentences = json.load(json_file)
-
-    example_webvtt_sr_model._request_caption_content = Mock(return_value=caption_text)
-
-    assert example_webvtt_sr_model._get_sentences("any-caption-uri") == timestamped_sentences
-
-
-def test_webvtt_sr_model_transcribe(example_webvtt_sr_model, fake_timestamped_sentences, tmpdir):
-    with open(fake_timestamped_sentences) as json_file:
-        timestamped_sentences = json.load(json_file)
-
-    example_webvtt_sr_model._get_sentences = Mock(return_value=timestamped_sentences)
 
     example_webvtt_sr_model.transcribe(
         "any-caption-uri",

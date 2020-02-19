b5acb478d229c95baedddd15fa300bf5c6dd9bbf
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-02-18 20:00:27 -0800

    ops/move-scraper-test-resources (#134)

--- cdptools/tests/event_scrapers/test_seattle_event_scraper.py
+++ cdptools/tests/event_scrapers/test_seattle_event_scraper.py
@@ -51,9 +51,11 @@ def test_clean_string(s, expected):
     assert actual == expected
 
 
-MOCK_SOUP_SINGLE_EVENT = ""
-with open("cdptools/tests/event_scrapers/test_data/seattle-channel.html") as markup:
-    MOCK_SOUP_SINGLE_EVENT = BeautifulSoup(markup.read(), features="html.parser")
+@pytest.fixture
+def single_event_soup(data_dir):
+    with open(data_dir / "example_seattle_channel_single_event.html", "r") as read_html:
+        return BeautifulSoup(read_html.read(), features="html.parser")
+
 
 MOCK_URI_SINGLE_EVENT = "http://www.seattlechannel.org/mayor-and-council/city-council/2018/2019-civic-development-public-assets-and-native-communities-committee/?videoid=x107461"  # noqa: E501
 EXPECTED_SINGLE_EVENT = {
@@ -65,10 +67,13 @@ EXPECTED_SINGLE_EVENT = {
 }
 
 
-def test_parse_single_seattle_channel_event():
-    event = SeattleEventScraper._parse_single_seattle_channel_event_by_main_content(MOCK_SOUP_SINGLE_EVENT,
-                                                                                    MOCK_URI_SINGLE_EVENT,
-                                                                                    ignore_date=True)
+def test_parse_single_seattle_channel_event(single_event_soup):
+    event = SeattleEventScraper._parse_single_seattle_channel_event_by_main_content(
+        single_event_soup,
+        MOCK_URI_SINGLE_EVENT,
+        ignore_date=True
+    )
+
     assert event == EXPECTED_SINGLE_EVENT
 
 
@@ -85,25 +90,3 @@ m_d_sy = "{}/{}/{}".format(now.month, now.day, str(now.year)[2:])
 MOCK_EVENT_HTML = """<div class="row borderBottomNone paginationItem"><div class="col-xs-12 col-sm-4 col-md-3"><a href="/testing?videoid=x99999" onclick="javascript:loadJWPlayer7('http://video.seattle.gov:8080/media/council/tests_032919.mp4','/images/seattlechannel/videoimages/channelGeneric.jpg', &quot;&lt;p&gt;The City of Seattle conducts a hearing on testing Council Data Project parsers. &lt;/p&gt;&lt;p&gt;&lt;/p&gt;&quot;, 'Example Body on Tests', '{m_d_sy}', '1:31:24', '9021807', false,'x93428', '', '', '', '', '', '', '', '', ''); return false;" target=""><img alt="Example Body on Tests {m_d_sy}" class="img-responsive" src="images/seattlechannel/videoimages/channelGeneric.jpg" title="Example Body on Tests {m_d_sy}"/></a></div><div class="col-xs-12 col-sm-8 col-md-9"><div class="titleDateContainer"><h2 class="paginationTitle"><a href="/testing?videoid=x99999" onclick="javascript:loadJWPlayer7('http://video.seattle.gov:8080/media/council/tests_032919.mp4','images/seattlechannel/videoimages/channelGeneric.jpg', &quot;&lt;p&gt;The City of Seattle conducts a hearing on testing Council Data Project parsers. &lt;/p&gt;&lt;p&gt;&lt;/p&gt;&quot;, 'Example Body on Tests', '{m_d_sy}', '1:31:24', '9021807', false,'x93428', '', '', '', '', '', '', '', '', ''); return false;" title="Example Body on Tests {m_d_sy}">Example Body on Tests</a></h2><div class="videoDate">{m_d_y}</div></div><div class="titleExcerptText"><p>Agenda: Public Comment; CB 999999.</p><p></p></div></div></div>""".format(m_d_y=m_d_y, m_d_sy=m_d_sy)  # noqa: E501
 MOCK_EVENT_SOUP = BeautifulSoup(MOCK_EVENT_HTML, "html.parser")
 SIBLING_ROUTE = "http://www.seattlechannel.org/CityCouncil"
-
-
-# def test_parse_seattle_channel_event():
-#     event = SeattleEventScraper._parse_seattle_channel_event(MOCK_EVENT_SOUP, SIBLING_ROUTE)
-#
-#     assert event == MOCK_EVENT
-
-
-# @pytest.mark.parametrize("a, b, expected", [
-#     (["Hello", "World"], ["Hello", "World"], True),
-#     (["Hello", "Test", "World"], ["Hello", "World"], True),
-#     (["Hello", "World"], ["Hello", "Test", "World"], True),
-#     (["Hello"], ["World"], False),
-#     (["World", "Hello"], ["Hello", "World"], False),
-#     (["This", "Is", "A", "Test"], ["Hello", "Is", "A", "World"], False)
-# ])
-# def test_two_items_list_match(a, b, expected):
-#     assert expected == SeattleEventScraper._shared_items_in_list_exist_and_ordered(a, b)
-
-
-# TODO:
-# Create tests for mock requests to get routes

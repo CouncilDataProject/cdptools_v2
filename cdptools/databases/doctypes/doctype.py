eaf4c7e72e1b44781ff5edcee2cb092f3f423bd4
osethan <GE47R9xmtouch .zshrcFASEPs@0>
2020-08-06 05:44:26 -0700

    Object string representation and equals update

--- cdptools/databases/doctypes/doctype.py
+++ cdptools/databases/doctypes/doctype.py
@@ -24,3 +24,12 @@ class Doctype(ABC):
         Convert custom object to schema dict.
         """
         return {}
+
+    def __str__(self):
+        return str(self.to_dict())
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, a, b):
+        return str(a) == str(b)
